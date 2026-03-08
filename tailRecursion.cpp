0fbaf930f42fca437b5de6b4c2b35e46c92093f1
agorbatova <anyutagorbatova@mail.ru>
2026-03-08 15:56:48 +1100

    no recursion was added

--- tailRecursion.cpp
+++ tailRecursion.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <iostream>
+#include <mach/mach.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
@@ -15,15 +16,26 @@ int sum(std::vector<int> a, int result = 0) {
 }
 
 int main() {
-  auto start = std::chrono::high_resolution_clock::now();
+  printf("TAIL RECURSION:\n");
   std::vector<int> n;
   int i = 1;
   while (i < 1024) {
     n.insert(n.begin(), i);
     i++;
   }
+  struct task_basic_info info;
+  mach_msg_type_number_t infoCount = TASK_BASIC_INFO_COUNT;
+  auto start = std::chrono::high_resolution_clock::now();
+
+  int sumV = sum(n);
+
   auto end = std::chrono::high_resolution_clock::now();
-  std::cout << "Sum " << "is " << sum(n) << "\n";
+  if (task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&info,
+                &infoCount) == KERN_SUCCESS) {
+    std::cout << "Memory: " << info.resident_size / 1024 / 1024 << " MB"
+              << std::endl;
+  }
+  std::cout << "Sum " << "is " << sumV << "\n";
 
   std::chrono::duration<double, std::milli> duration_ms = end - start;
   std::cout << "Execution time: " << duration_ms.count() << " milliseconds\n";
